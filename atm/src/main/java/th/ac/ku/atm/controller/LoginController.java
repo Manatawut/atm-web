084aa7d6acd3a51d5b9bfb80d3c9d5ddcb30d832
Manatawut <manatawut@gmail.com>
2021-03-19 00:21:06 +0700

    Add BankAccountPage

--- atm/src/main/java/th/ac/ku/atm/controller/LoginController.java
+++ atm/src/main/java/th/ac/ku/atm/controller/LoginController.java
@@ -7,17 +7,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import th.ac.ku.atm.model.Customer;
+import th.ac.ku.atm.service.BankAccountService;
 import th.ac.ku.atm.service.CustomerService;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
     private CustomerService customerService;
+    private BankAccountService bankAccountService;
 
-    public LoginController(CustomerService customerService) {
+    public LoginController(CustomerService customerService, BankAccountService bankAccountService) {
         this.customerService = customerService;
+        this.bankAccountService = bankAccountService;
     }
-
 
     @GetMapping
     public String getLoginPage() {
@@ -32,8 +34,11 @@ public class LoginController {
 
         //2. if match, welcome customer
         if (matchingCustomer != null) {
-            model.addAttribute("greeting",
-                    "Welcome, " + matchingCustomer.getName());
+            model.addAttribute("customertitle",
+                    matchingCustomer.getName() + " Bank Accounts");
+            model.addAttribute("bankaccounts",
+                    bankAccountService.getCustomerBankAccount(customer.getId()));
+            return "customeraccount";
         } else {
         //3. not match, display that customer info is incorrect
             model.addAttribute("greeting", "Can't find customer");
